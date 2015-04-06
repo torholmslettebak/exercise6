3884fbc0e8cfb0a0b754bf6106c2b9e1901468c5
torholmslettebak <torholmslettebak@gmail.com>
2015-04-06 13:48:15 +0200

    removed _build files

--- poisson1d/_build/CMakeFiles/poisson_common.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-FILE(REMOVE_RECURSE
-  "libpoisson_common.a"
-)
--- poissonRewritten/_build/common/CMakeFiles/verbose-test.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/verbose-test"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/verbose-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
